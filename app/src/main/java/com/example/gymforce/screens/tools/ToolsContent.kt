46775aa84e359ffcc8a950b348a76e71bf80d12d
Abdallah Alnajar <140744532+abdallahalnagar188@users.noreply.github.com>
2024-04-25 17:40:28 -1200

    training type

--- app/src/main/java/com/example/gymforce/screens/tools/ToolsContent.kt
+++ app/src/main/java/com/example/gymforce/screens/tools/ToolsContent.kt
@@ -6,9 +6,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 
 @Composable
-fun ToolsContent() {
+fun ToolsContent(navHostController: NavHostController) {
     Column(modifier = Modifier.fillMaxSize(),Arrangement.Center) {
         Text(text = "Tools Screen")
 
