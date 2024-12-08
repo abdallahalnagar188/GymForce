89dd4486dc34c3a9041d3d0c49d1884b54555396
Abdallah Alnajar <140744532+abdallahalnagar188@users.noreply.github.com>
2024-12-08 16:31:51 +0200

    update version for make update in store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.example.gymforce"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
